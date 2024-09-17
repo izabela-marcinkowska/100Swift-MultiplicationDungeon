8f52ab09f70f740e6b78b7cbb3d62d4f17bb7857
Devy <izabela.marcinkowska@outlook.com>
2024-09-17 15:00:37 +0200

    feat: Create multiplication function

--- MultiplicationDungeon/ContentView.swift
+++ MultiplicationDungeon/ContentView.swift
@@ -7,17 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var wishedAmountQuestions = 10
+    @State private var wishedLevel = 5
+    @State private var questions = [Int]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button ("Click here") {
+                createQuestions(AmountQuestions: wishedAmountQuestions, level: wishedLevel)
+            }
+            List {
+                Section("Section 2") {
+                    ForEach(questions, id: \.self) {
+                        Text("Dynamic row \($0)")
+                    }
+                }
+                
+                
+            }
         }
-        .padding()
+    }
+    
+    func createQuestions (AmountQuestions: Int, level: Int) -> Void {
+        for _ in 1...AmountQuestions {
+            let firstNumber = Int.random(in: 1...wishedLevel)
+            let secondNumber = Int.random(in: 1...wishedLevel)
+            let result = firstNumber * secondNumber
+            print(firstNumber, secondNumber, result)
+            questions.append(result)
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
