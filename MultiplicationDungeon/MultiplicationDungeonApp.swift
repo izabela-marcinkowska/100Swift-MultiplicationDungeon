278828496611a3d928722a4dd3c93a7cc4810924
Devy <izabela.marcinkowska@outlook.com>
2024-09-17 14:19:08 +0200

    First commit

---
+++ MultiplicationDungeon/MultiplicationDungeonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationDungeonApp.swift
+//  MultiplicationDungeon
+//
+//  Created by Izabela Marcinkowska on 2024-09-17.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationDungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
